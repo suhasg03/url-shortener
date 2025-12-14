7eb76e4b08e9732635dadd36651532e19e423ecf
Suhas <suhas.03su@gmail.com>
2025-12-14 17:44:48 +0530

    feat: removed main.go and made a package

--- custom_encoding.go
+++ custom_encoding.go
@@ -1,27 +1,13 @@
-package main
+package urlshortener
 
-// import (
-// 	"fmt"
-// 	"strconv"
-// )
+import (
+	"strconv"
+)
 
-// func encodeXOR(n int64, key byte) string {
-// 	bytes := make([]byte, 8)
-// 	for i := 0; i < 8; i++ {
-// 		bytes[i] = byte(n>>(i*8)) ^ key
-// 	}
-// 	return fmt.Sprintf("%08x", bytes) // Hex string
-// }
+func EncodeBase36(num int64) (string, error) {
+	return strconv.FormatInt(num, 36), nil
+}
 
-// func decodeXOR(s string, key byte) (int64, error) {
-// 	bytes := make([]byte, 8)
-// 	for i := 0; i < 8; i++ {
-// 		b, _ := strconv.ParseUint(s[i*2:i*2+2], 16, 8)
-// 		bytes[i] = byte(b) ^ key
-// 	}
-// 	var result int64
-// 	for i := 0; i < 8; i++ {
-// 		result |= int64(bytes[i]) << (i * 8)
-// 	}
-// 	return result, nil
-// }
+func DecodeBase36(s string) (int64, error) {
+	return strconv.ParseInt(s, 36, 64)
+}
--- url_shortener.go
+++ url_shortener.go
@@ -1,7 +1,44 @@
-package main
+package urlshortener
 
-// func ShortenUrl(url string) *string {
-// 	id := GetPsuedoRandomIntegerId()
-// 	s := encodeXOR(id, 0x5A)
-// 	return &s
-// }
+import (
+	"database/sql"
+	"fmt"
+)
+
+func ShortenUrl(url string, db *sql.DB) *string {
+	id := GetPsuedoRandomIntegerId()
+	s, err := EncodeBase36(id)
+	if err != nil {
+		return nil
+	}
+	err = insertDataToDB(db, s, url)
+	if err != nil {
+		return nil
+	}
+	return &s
+}
+
+func insertDataToDB(db *sql.DB, shortcode string, url string) error {
+	_, err := db.Exec("INSERT INTO shortenedurl (shortcode, url) VALUES (?, ?)", shortcode, url)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetOriginalUrl(shortcode string, db *sql.DB) (*string, error) {
+	rows, err := db.Query("SELECT url FROM shortenedurl WHERE shortcode = ?", shortcode)
+	if err != nil {
+		return nil, err
+	}
+	var urls []string
+	for rows.Next() {
+		var url string
+		rows.Scan(&url)
+		urls = append(urls, url)
+	}
+	if len(urls) == 0 {
+		return nil, fmt.Errorf("no URL found for shortcode: %s", shortcode)
+	}
+	return &urls[0], nil
+}
